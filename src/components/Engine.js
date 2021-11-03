ff4ee82b499182a8a70047910ea636805dc5d2c0
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2021-11-03 15:38:43 +0530

    Video Complete

--- src/components/Engine.js
+++ src/components/Engine.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import mockData from '../data/MOCK_DATA.json'
-import './Engine.css'
+import React, {useState} from "react";
+import mockData from "../data/MOCK_DATA.json";
+import "./Engine.css";
 
 const Engine = () => {
 
-    return (
-        <div className="engine-div">
-            <div className="mb-2" style={{width: '40%', marginTop: 20}}>
-                <input type="text" className="form-control" placeholder="Enter the term" />
-            </div>
-        </div>
-    )
-}
+    const [searchTerm, setSearchTerm] = useState('')
 
-export default Engine
+    console.log(mockData)
+
+  return (
+    <div className="engine-div">
+      <div className="mb-2" style={{ width: "40%", marginTop: 20 }}>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter the term"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm}
+        />
+      </div>
+      {
+          mockData.filter((item) => {
+              if(searchTerm === '' || searchTerm === null){
+                  return item
+              }
+              else{
+                  return item.first_name.toLowerCase().includes(searchTerm.toLowerCase())
+              }
+          }).map((item) => (
+              <p key={item.id}>{item.first_name}{" "}{item.last_name}</p>
+          ))
+      }
+    </div>
+  );
+};
+
+export default Engine;
